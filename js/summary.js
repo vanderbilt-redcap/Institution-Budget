e3dd3d794ce5d2452334fe93c8f815d9a05472cc
Carl Reed <carlreedw@gmail.com>
2021-06-09 15:26:37 -0500

    adds sum row to procedure costs summary review table, css improvements

--- js/summary.js
+++ js/summary.js
@@ -25,7 +25,6 @@ TINSummary.addFixedCostsTable = function(parent) {
 		return;
 	var table_html = "";
 	var data = TINSummary.summary_data;
-	var arms = TINSummary.schedule.arms;
 	
 	table_html += "<h4 class='summary-table-header'>FIXED COSTS SUMMARY REVIEW</h4>";
 	table_html += "\
@@ -41,12 +40,15 @@ TINSummary.addFixedCostsTable = function(parent) {
 		<tbody>";
 	
 	var i;
-	for (i = 1; i <= arms.length; i++) {
+	for (i = 1; i <= 5; i++) {
+		// convert decision to GO or NO-GO or NEED INFO
+		var result = TINSummary.convertDecision(data["arm" + i + "_decision"]);
+		
 		table_html += "\
-			<tr>\
+			<tr" + result.row_class + ">\
 				<td>" + data["fixedcost" + i] + "</td>\
 				<td>" + data["fixedcost" + i + "_detail"] + "</td>\
-				<td>" + data["fixedcost" + i + "_decision"] + "</td>\
+				<td>" + result.decision + "</td>\
 				<td>" + data["fixedcost" + i + "_comments"] + "</td>\
 			</tr>";
 	}
@@ -80,20 +82,39 @@ TINSummary.addProcedureCostsTable = function(parent) {
 		<tbody>";
 	
 	var i;
+	var cc_total = 0;
+	var site_total = 0;
 	for (i = 1; i <= arms.length; i++) {
-		// var arm_name = "Arm " + i + ": ";
 		var arm_name = "Arm " + i + ": " + arms[i-1].name;
 		var cc_cost = TINSummary.getCoordCenterCost(i);
 		var site_cost = TINSummary.getSiteCost(i);
+		
+		// add costs to cc/site totals
+		cc_total += cc_cost;
+		site_total += site_cost;
+		
+		// convert decision to GO or NO-GO or NEED INFO
+		var result = TINSummary.convertDecision(data["arm" + i + "_decision"]);
+		
 		table_html += "\
-			<tr>\
+			<tr" + result.row_class + ">\
 				<td>" + arm_name + "</td>\
-				<td>" + cc_cost + "</td>\
-				<td>" + site_cost + "</td>\
-				<td>" + data["arm" + i + "_decision"] + "</td>\
+				<td>$" + cc_cost + "</td>\
+				<td>$" + site_cost + "</td>\
+				<td>" + result.decision + "</td>\
 				<td>" + data["arm" + i + "_comments"] + "</td>\
 			</tr>";
 	}
+	
+	// append sum row
+	table_html += "\
+			<tr>\
+				<td>SUM AMOUNT</td>\
+				<td>$" + cc_total + "</td>\
+				<td>$" + site_total + "</td>\
+				<td>--</td>\
+				<td>--</td>\
+			</tr>";
 	
 	table_html += "\
 		</tbody>\
@@ -131,6 +152,24 @@ TINSummary.getSiteCost = function(arm_index) {
 		}
 	}
 	return total;
+}
+
+TINSummary.convertDecision = function(decision) {
+	var decision = decision;
+	var row_class = '';
+	if (decision == 'accept') {
+		decision = 'GO';
+	} else if (decision == 'unable to accept') {
+		decision = 'NO-GO';
+		row_class = " class='no-go'";
+	} else if (decision == 'request additional information') {
+		decision = 'NEED INFO';
+	}
+	
+	return {
+		decision: decision,
+		row_class: row_class
+	};
 }
 
 $('head').append('<link rel="stylesheet" type="text/css" href="' + TINSummary.css_url + '">');
