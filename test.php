d7846a0f65877c1af02ef952725fb0e34aebdfae
Carl Reed <carl.w.reed@vumc.org>
2021-10-08 13:40:39 -0500

    deleted test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
-
-echo("<pre>");
-print_r($module->createSiteInstances('1'));
-echo("</pre>");
-
-require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
